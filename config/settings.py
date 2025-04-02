10f352492b8aa4462e0ed82f263578bb30133a55
CristanMedina <crishighlights@gmail.com>
2025-04-02 11:43:15 -0700

    Agregado el modelo de users. Tambien las apps instaladas en settings.

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kitchen_orders',
+    'menu',
+    'orders',
+    'users',
 ]
 
 MIDDLEWARE = [
