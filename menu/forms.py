6152f420faa17c17d707e0fb29a226f875f794fd
AlexQuezada24 <jorge.quezadatrelles@cesunbc.edu.mx>
2025-04-03 22:54:16 -0700

    Update forms.py

--- menu/forms.py
+++ menu/forms.py
@@ -4,7 +4,7 @@ from .models import Platillo
 class MenuForm(forms.ModelForm):
     class Meta:
         model = Platillo
-        fields = ['nombre', 'descripcion', 'precio', 'disponible', 'ingredientes']
+        fields = ['nombre', 'descripcion', 'precio', 'disponible', 'ingredientes', 'imagen']
         widgets = {
             'descripcion': forms.Textarea(attrs={'rows': 3}),
             'ingredientes': forms.Textarea(attrs={'rows': 3}),
